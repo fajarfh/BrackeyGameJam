40dd59437577eebeb94863e67e4d01b3e5b29e44
fajarfh <roseorchiddawn@gmail.com>
2025-08-24 18:42:40 +0700

    Initial
    Inisialisasi dan snippet dari Portal Potty

---
+++ scripts/game_manager.gd
@@ -0,0 +1,51 @@
+extends Node3D
+
+@export var finish_line: Area3D
+
+@export var main_panel: Control
+@export var guide_panel: Control
+@export var credits_panel: Control
+@export var map_panel: Control
+@export var win_panel: Control
+
+
+func _ready():
+	get_tree().paused = true
+	#if get_tree().paused:
+		#get_tree().paused = true
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("open_map"):
+		map_panel.show()
+		get_tree().paused = true
+		
+	if Input.is_action_just_released("open_map"):
+		map_panel.hide()
+		get_tree().paused = false
+
+func _on_restart_pressed():
+	#await get_tree().create_timer(2.0).timeout
+	get_tree().reload_current_scene()
+	
+	
+
+func _on_win_line_body_entered(body):
+	if body is CharacterBody3D:
+		win_panel.show()
+		get_tree().paused = true
+
+
+func _on_guide_pressed():
+	main_panel.hide()
+	guide_panel.show()
+
+
+func _on_start_pressed():
+	main_panel.hide()
+	guide_panel.hide()
+	get_tree().paused = false
+
+
+func _on_credits_pressed():
+	win_panel.hide()
+	credits_panel.show()
---
+++ scripts/player.gd
@@ -0,0 +1,64 @@
+extends CharacterBody3D
+
+@export_range(0.0, 50.0, 0.5) var min_speed:float = 0.0
+@export_range(30.0, 100.0, 0.5) var max_speed:float = 50
+@export_range(0.0, 1.0, 0.05) var turn_speed:float = 0.75
+@export_range(0.0, 1.0, 0.05) var pitch_speed:float = 0.5
+@export_range(0.0, 10.0, 0.1) var level_speed:float = 3.0
+@export_range(10.0, 100.0, 1.0) var throttle_delta:float = 30.0
+@export_range(0.0, 10.0, 0.1) var acceleration:float = 6.0
+@export_range(1.0, 10.0, 1.0) var snap_speed:float = 5.0
+
+@export var mesh_body : Node3D
+@export var player_cam:Camera3D
+
+
+var forward_speed:float = 0
+var target_speed:float = 0
+
+var turn_input:float= 0
+var pitch_input:float= 0
+
+
+@onready var animation_tree = %AnimationTree
+@onready var state_machine : AnimationNodeStateMachinePlayback = animation_tree.get("parameters/StateMachine/playback")
+
+func get_input(delta):
+	if Input.is_action_pressed("throttle_up"):
+		target_speed = min(forward_speed + throttle_delta * delta, max_speed)
+	else:
+		target_speed = max(forward_speed - throttle_delta * delta, min_speed)
+	if Input.is_action_pressed("throttle_down"):
+		target_speed = max(forward_speed - throttle_delta * 2 * delta, min_speed)
+	
+	turn_input = Input.get_action_strength("roll_left") - Input.get_action_strength("roll_right")
+	pitch_input = Input.get_action_strength("pitch_up") - Input.get_action_strength("pitch_down")
+	
+func _physics_process(delta):
+	get_input(delta)
+	transform.basis = transform.basis.rotated(transform.basis.x, pitch_input*pitch_speed*delta)
+	if pitch_input == 0:
+		mesh_body.rotation.x = lerp(mesh_body.rotation.x,pitch_input,level_speed * snap_speed * delta)
+		idle()
+	else:
+		moving()
+		mesh_body.rotation.x = lerp(mesh_body.rotation.x,pitch_input,level_speed * delta)
+		
+	transform.basis = transform.basis.rotated(Vector3.UP, turn_input*turn_speed*delta)
+	
+	if turn_input == 0:
+		mesh_body.rotation.y = lerp(mesh_body.rotation.y,turn_input,level_speed * snap_speed * delta)
+		idle()
+	else:
+		moving()
+		mesh_body.rotation.y = lerp(mesh_body.rotation.y,turn_input,level_speed * delta)
+	
+	forward_speed = lerp(forward_speed, target_speed, acceleration*delta)
+	velocity = -transform.basis.z * forward_speed
+	move_and_slide()
+
+func moving():
+	state_machine.travel("static")
+
+func idle():
+	state_machine.travel("idle")
