1a9a26d42a74d12d83be94d6990e90f2ac1b829e
fajarfh <roseorchiddawn@gmail.com>
2025-08-28 23:18:34 +0700

    Update winning condition
    Sistem spawn objek
    win/lose condition
    UI

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,9 +5,18 @@ extends Node3D
 @export var credits_panel: Control
 @export var map_panel: Control
 @export var win_panel: Control
+@export var gameover_panel: Control
+@export var hud_main: Control
 @export var hud_oob: Control
+@export var hud_biscuit: Control
 
 @export var spawner:Node
+@export var player:CharacterBody3D
+
+var biscuit_score = 0:
+	set(score):
+		biscuit_score = score
+		hud_biscuit.text = str(biscuit_score) + "/" + str(spawner.biscuit_spawn_number)
 
 
 func _ready():
@@ -15,6 +24,11 @@ func _ready():
 	AudioControl.playBgm("main")
 	#if get_tree().paused:
 		#get_tree().paused = true
+		
+	player.connect("dying_char", game_over)
+	spawner.connect("biscuit_picked", add_biscuit)
+	hud_main.hide()
+	biscuit_score = 0
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("open_map"):
@@ -45,12 +59,14 @@ func _on_guide_pressed():
 func _on_start_pressed():
 	main_panel.hide()
 	guide_panel.hide()
+	hud_main.show()
 	spawner.reset_area()
 	get_tree().paused = false
 
 
 func _on_credits_pressed():
 	win_panel.hide()
+	gameover_panel.hide()
 	credits_panel.show()
 
 
@@ -67,3 +83,21 @@ func _on_soft_bound_body_exited(body):
 	else:	
 		hud_oob.show() #hud_oob.hide() # Replace with function body.
 		
+func game_over():
+	#print("yup game over")
+	hud_main.hide()
+	await get_tree().create_timer(2.0).timeout
+	get_tree().paused = true
+	gameover_panel.show()
+
+func add_biscuit():
+	biscuit_score+=1
+	if biscuit_score >= spawner.biscuit_spawn_number:
+		winning()
+
+func winning():
+	
+	await get_tree().create_timer(1.0).timeout
+	hud_main.hide()
+	win_panel.show()
+	get_tree().paused = true
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -18,14 +18,25 @@ var enemies = []
 var obstacles = []
 var biscuits = []
 
+signal biscuit_picked
+
 func _ready():
+	
+	for k in range(0,biscuit_spawn_number):
+		spawn_item(biscuit_spawn_points.pick_random(),biscuit, biscuits, "biscuit")
+	
 	for i in range(0,enemy_spawn_number):
-		spawn_item(enemy_spawn_points[0],enemy, enemies, "enemy")
+		spawn_item(enemy_spawn_points.pick_random(),enemy, enemies, "enemy")
+	
+	for j in range(0,obstacle_spawn_number):
+		spawn_item(obstacle_spawn_points.pick_random(),obstacle.pick_random(), obstacles, "obstacle")
+	
+
 
 func spawn_item(parent:Node3D, item:Resource, container:Array, name_suffix:String):
 
 	var item_new:Node3D = load(item.resource_path).instantiate()
-	var area_body:Area3D = item_new.get_node("%static")
+	var area_body:Area3D = item_new.get_node("%area_overlap")
 	parent.add_child(item_new)
 	item_new.position = random_loc(parent)
 	item_new.name = name_suffix + "_" + str(len(container))
@@ -37,8 +48,27 @@ func spawn_item(parent:Node3D, item:Resource, container:Array, name_suffix:Strin
 func reposition(body, node, parent):
 	node.position = random_loc(parent)
 	
+func enm_collision(body):
+	if body is CharacterBody3D:
+		body.dying_prep()
+		
+	elif "bullet" in body.name:
+		await get_tree().create_timer(2.0).timeout
+		spawn_item(enemy_spawn_points.pick_random(), enemy, enemies, "enemy")
+		var area_body:Area3D = enemies[-1].get_node("%area_overlap")
+		area_body.disconnect("body_entered",reposition)
+		area_body.disconnect("area_entered",reposition)
+		
+		var enm_bod:Area3D = enemies[-1].get_node("%enm_bod")
+		enm_bod.connect("body_entered", enm_collision)
+
+func itm_picked(body, node):
+	if body is CharacterBody3D:
+		biscuit_picked.emit()
+		node.queue_free()
+	
 func random_loc(parent:Node3D) -> Vector3:
-	var random_r = randf_range(0,enemy_spawn_points[0].radius)
+	var random_r = randf_range(0,parent.radius)
 	var random_alpha = randf_range(0,2*PI)
 	var random_beta = randf_range(0,2*PI)
 	
@@ -54,6 +84,20 @@ func car2pol(r, alpha, beta) -> Vector3:
 
 func reset_area():
 	for item in enemies:
-		var area_body:Area3D = item.get_node("%static")
+		var area_body:Area3D = item.get_node("%area_overlap")
+		area_body.disconnect("body_entered",reposition)
+		area_body.disconnect("area_entered",reposition)
+		
+		var enm_bod:Area3D = item.get_node("%enm_bod")
+		enm_bod.connect("body_entered", enm_collision)
+
+	for item in biscuits:
+		var area_body:Area3D = item.get_node("%area_overlap")
+		area_body.disconnect("body_entered",reposition)
+		area_body.disconnect("area_entered",reposition)
+		area_body.connect("body_entered",itm_picked.bind(item))
+		
+	for item in obstacles:
+		var area_body:Area3D = item.get_node("%area_overlap")
 		area_body.disconnect("body_entered",reposition)
 		area_body.disconnect("area_entered",reposition)
